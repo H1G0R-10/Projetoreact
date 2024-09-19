95f8e48f0377969ffd409daf9521c3285981b3e3
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-19 17:03:03 -0300

    Primeiro commit do repositório

---
+++ Projetoreact/src/main.jsx
@@ -0,0 +1,8 @@
+
+import { createRoot } from 'react-dom/client'
+import HelloWorld from './HelloWorld'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <HelloWorld />
+)
